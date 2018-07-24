ced09837b737528bfb8988e065e00ebacfcf852e
Rafael Lopez <lpz.raf@gmail.com>
2018-07-24 03:10:24 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -15,5 +15,5 @@ end
 
 def find_the_cheese(array)
   cheesy = ["gouda", "cheddar", "camembert"]
-  array.any? {|cheese| cheesy.include?(cheese)} 
+  array.find? {|cheese| cheesy.include?(cheese)} 
     end
