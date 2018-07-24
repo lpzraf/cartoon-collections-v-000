35ee93fafa803f00b481592a7efdfa60c2afac63
Rafael Lopez <lpz.raf@gmail.com>
2018-07-24 02:30:53 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,7 +13,8 @@ def long_planeteer_calls(calls_long)
    calls_long.length > 4 ? true : false
 end
 
-def find_the_cheese()
+def find_the_cheese(cheddar_cheese)
+  
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
